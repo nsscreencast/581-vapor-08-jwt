937d5b6a47a6a9a53ae5c6903ea527feaae44186
Ben Scheirman <ben@scheirman.com>
2024-07-26 07:55:16 -0500

    Episode 581

---
+++ Sources/App/configure.swift
@@ -0,0 +1,62 @@
+import NIOSSL
+import Fluent
+import FluentPostgresDriver
+import JWT
+import Vapor
+
+private enum AppError: Error {
+    case missingDatabaseCredentials
+    case missingJWTSigningSecret
+}
+
+// configures your application
+public func configure(_ app: Application) async throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    try await configureDatabase(app)
+
+    guard let signingSecret = Environment.get("JWT_SIGNING_SECRET") else {
+        throw AppError.missingJWTSigningSecret
+    }
+    await app.jwt.keys.add(hmac: HMACKey(from: signingSecret), digestAlgorithm: .sha256)
+
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    decoder.dateDecodingStrategy = .iso8601
+
+    let encoder = JSONEncoder()
+    encoder.keyEncodingStrategy = .convertToSnakeCase
+    encoder.dateEncodingStrategy = .iso8601
+    encoder.outputFormatting = .prettyPrinted
+
+    ContentConfiguration.global.use(decoder: decoder, for: .json)
+    ContentConfiguration.global.use(encoder: encoder, for: .json)
+}
+
+private func configureDatabase(_ app: Application) async throws {
+    guard
+        let dbUser = Environment.get("DATABASE_USERNAME"),
+        let dbPass = Environment.get("DATABASE_PASSWORD"),
+        let dbName = Environment.get("DATABASE_NAME")
+    else {
+        throw AppError.missingDatabaseCredentials
+    }
+
+    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        username: dbUser,
+        password: dbPass,
+        database: dbName,
+        tls: .prefer(try .init(configuration: .clientDefault)))
+    ), as: .psql)
+
+    app.migrations.add(CreateBands())
+    app.migrations.add(CreateSongs())
+    app.migrations.add(CreateArtists())
+    app.migrations.add(CreateUsers())
+
+    // register routes
+    try routes(app)
+}
